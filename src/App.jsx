f5de5358cc668e82b6a858fb5bd02cb3eca6dcf3
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-02-20 23:06:07 +0530

    created currency converter

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import CurrencyConverter from './components/currencyconverter'
+function App() {
+
+  return (
+    <>
+      <CurrencyConverter />
+    </>
+  )
+}
+
+export default App
